021f4ee8d3517b0aa1bde5b97aa4c7de7e593a6a
Andrey Shutov <220919@edu.fa.ru>
2025-04-10 17:46:33 +0300

    Fixed issue with the podman script

--- scripts/run-report-build-container.sh
+++ scripts/run-report-build-container.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # REPORT_NAME=report-image
+REPORT_NAME=ghcr.io/quarto-dev/quarto:1.7.19
 # podman build ./doc/misc/ --tag $REPORT_NAME
 podman run -it -e TYPST_FONT_PATHS=./ -v ./doc/report/:/root/ -w=/root/ $REPORT_NAME quarto render; rm ./.bash_history
