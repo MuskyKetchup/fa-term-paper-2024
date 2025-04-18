2f60aec904ea12dab73bbef409f210eb3699a622
Andrey Shutov <220919@edu.fa.ru>
2025-04-18 15:38:52 +0300

    Fixed doc build script to work with current report

--- scripts/run-report-build-container.sh
+++ scripts/run-report-build-container.sh
@@ -2,4 +2,5 @@
 # REPORT_NAME=report-image
 REPORT_NAME=ghcr.io/quarto-dev/quarto:1.7.19
 # podman build ./doc/misc/ --tag $REPORT_NAME
-podman run -it -e TYPST_FONT_PATHS=./ -v ./doc/report/:/root/ -w=/root/ $REPORT_NAME quarto render; rm ./.bash_history
+cd $(dirname $0)/../
+podman run -it -e TYPST_FONT_PATHS=/root/doc/report -v ./:/root/ -w=/root/doc/report $REPORT_NAME quarto render; 
